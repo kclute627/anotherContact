1b8bce6aa561405a0d5c8e64f80d091944687f7b
Kyle C <kyclutter@gmail.com>
2019-09-03 12:57:28 -0500

    up date

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import './scss/main.scss';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-// import './scss/main.scss';
+
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
