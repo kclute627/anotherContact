17a79c0e39cad744739f84eea147f5b0563d2d67
Kyle C <kyclutter@gmail.com>
2019-09-03 12:24:45 -0500

    update

--- src/App.js
+++ src/App.js
@@ -1,13 +1,71 @@
-import React from 'react';
-import logo from './logo.svg';
-import Contact from './Components/contact';
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <Contact/>
-    </div>
-  );
+
+class App extends Component {
+
+  state={
+    name: '',
+    email: '',
+    message: '',
 }
+
+changeHandler= e => {
+    this.setState({
+        [e.target.name]: e.target.value,
+    })
+}
+submitHandler = (e)=> {
+    // runnug
+    e.preventDefault()
+    this.setState({
+        name: '',
+        email: '',
+        message: '',
+    })
+}
+
+
+render(){
+    const {name, email, message}= this.state;
+
+
+    return(
+        <div className="contact-container">
+            <form action="" className="contact-form">
+                <label htmlFor="" className="contact-form__label">
+                    Please provide your Name 
+                </label>
+                <input 
+                name="name" 
+                type="text" 
+                placeholder="Name" 
+                className="contact-form__name input"
+                value={name}
+                onChange= {this.changeHandler}/>
+                <label htmlFor="" className="contact-form__label">Email Address</label>
+                <input  
+                name="email" 
+                type="email"  
+                placeholder="Email" 
+                className="contact-form__email input"
+                value={email}
+                onChange= {this.changeHandler}/>
+                <label htmlFor="" className="contact-form__label">Leave Us a Note </label>
+                <textarea 
+                name="message" 
+                className="contact-form__message"
+                value={message}
+                onChange= {this.changeHandler}
+                />
+                <input type="submit" value="Submit" className="contact-form__submit" onClick={this.submitHandler}/>
+                <input type="hidden" name="form-name" value="contact"/>
+            </form>
+
+        </div>
+    )
+}
+}
+
+
 
 export default App;
