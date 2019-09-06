43b683200185098453099aac86a5141afb3fa704
Kyle C <kyclutter@gmail.com>
2019-09-06 09:51:25 -0500

    updates

--- src/Components/contact.js
+++ src/Components/contact.js
@@ -32,33 +32,34 @@ class Contact extends Component {
         return(
             <div className="contact-container">
                 <form name='contact' className="contact-form" method='post'>
+                <input type="hidden" name="form-name" value="contact"/>
                     <label htmlFor="" className="contact-form__label">
-                        Please provide your Name 
-                    </label>
-                    <input 
+                        Please provide your Name:  <input 
                     name="name" 
                     type="text" 
                     placeholder="Name" 
                     className="contact-form__name input"
                     value={name}
                     onChange= {this.changeHandler}/>
-                    <label htmlFor="" className="contact-form__label">Email Address</label>
-                    <input  
+                    </label>
+                   
+                    <label htmlFor="" className="contact-form__label">Email Address: <input  
                     name="email" 
                     type="email"  
                     placeholder="Email" 
                     className="contact-form__email input"
                     value={email}
-                    onChange= {this.changeHandler}/>
-                    <label htmlFor="" className="contact-form__label">Leave Us a Note </label>
-                    <textarea 
+                    onChange= {this.changeHandler}/></label>
+                    
+                    <label htmlFor="" className="contact-form__label">Leave Us a Note: <textarea 
                     name="message" 
                     className="contact-form__message"
                     value={message}
                     onChange= {this.changeHandler}
-                    />
+                    /> </label>
+                    
                     <input type="submit" value="Submit" className="contact-form__submit" onClick={this.submitHandler}/>
-                    <input type="hidden" name="form-name" value="contact"/>
+                    
                 </form>
 
             </div>
